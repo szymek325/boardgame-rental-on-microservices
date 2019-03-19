c4071d6297c59bd3af23f17934e77afbd0ec2fb8
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2019-03-19 09:21:08 +0100

    entities upgrade, first data Access project added

---
+++ BoardGameRentalApp.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BoardGameRentalApp.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
