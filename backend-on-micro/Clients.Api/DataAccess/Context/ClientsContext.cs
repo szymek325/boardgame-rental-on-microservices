8f2cb1292770f0621fc30c685f315e6ce8ed1dfd
szymek325 <matszym325@gmail.com>
2019-07-06 12:28:49 +0200

    db added

---
+++ backend-on-micro/Clients.Api/DataAccess/Context/ClientsContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Clients.Api.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clients.Api.DataAccess.Context
+{
+    public class ClientsContext : DbContext
+    {
+        public ClientsContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("c");
+            modelBuilder.Entity<Client>().HasKey(x => x.Id);
+            modelBuilder.Entity<Client>().Property(x => x.CreationTimeUtc).HasDefaultValue(DateTime.UtcNow);
+        }
+    }
+}
