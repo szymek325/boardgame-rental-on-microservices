df1a7d4dc982053d34bbae2d82803108715cf9ec
szymek325 <matszym325@gmail.com>
2019-07-10 11:41:19 +0200

    Clients.Api with mssql linux in docker

---
+++ backend-on-micro/Clients.Api/Migrations/20190710092755_InitDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clients.Api.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    ContactNumber = table.Column<string>(nullable: true),
+                    CreationTimeUtc = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2019, 7, 10, 9, 27, 55, 134, DateTimeKind.Utc).AddTicks(6720))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
