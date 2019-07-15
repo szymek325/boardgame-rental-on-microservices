b034ba3f67d95160a968874d3a5e8f7f4e27a5eb
szymek325 <matszym325@gmail.com>
2019-07-15 07:39:35 +0200

    web project and rabbitmq added to docker

---
+++ backend-on-micro/Web/Web.Mvc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Base.Configuration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NLog;
+using NLog.Web;
+
+namespace Web.Mvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = CommonProgram.CreateConfigurationBuilder().Build();
+            var logger = CommonProgram.GetLogger(configuration);
+
+            try
+            {
+                logger.Debug("Application started");
+                CreateWebHostBuilder(args)
+                    .UseConfiguration(configuration)
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception when building WebHost");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseNLog();
+        }
+    }
+}
