bc71971f05cf401673b478cf1174b14ddb974c0b
szymek325 <matszym325@gmail.com>
2019-08-18 10:55:50 +0200

    branch fixed after rebase on develop

---
+++ src/Services/Clients.Api/Handlers/SendMessageEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Base.RabbitMq.Markers;
+using Base.RabbitMq.Messages;
+
+namespace Clients.Api.Handlers
+{
+    public class SendMessageEventHandler : IEventHandler<SendEvent>
+    {
+        public async Task HandleAsync(SendEvent @event, CancellationToken token)
+        {
+            Console.WriteLine($"Receive: {@event.Message}");
+        }
+    }
+}
